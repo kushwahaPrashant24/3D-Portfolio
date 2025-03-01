8719d70cc00fd04893956d76a9a210aaae082bdc
kushwahaPrashant24 <146343202+kushwahaPrashant24@users.noreply.github.com>
2025-03-01 19:14:44 +0530

    added my new working experience

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -26,8 +26,7 @@ const Hero = () => {
           </p>
         </div>
       </div>
-
-      <ComputersCanvas />
+<ComputersCanvas />
 
       <div className='absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center'>
         <a href='#about'>
